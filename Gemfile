02bfc423301ad2e82fef06780c5e7d453132c25a
Matthew Lyons <matthew@nowaffles.com>
2022-03-22 13:44:25 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in rdiscord-sdk.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+gem "rice", "~> 4.0.3"
+gem "rubocop", "~> 1.21"
---
+++ ext/rdiscord_sdk/extconf.rb
@@ -0,0 +1,15 @@
+require "mkmf-rice"
+
+THIRDPARTY_DIR = File.join(FIle.dirname(__FILE__), "../../third_party")
+HEADERS = File.join(THIRDPARTY_DIR, "include")
+LIBRARY_DIR = if RUBY_PLATFORM.start_with?("x86_64") or RUBY_PLATFORM.start_with?("x64")
+                File.join(THIRDPARTY_DIR, "lib/x86_64")
+              else
+                puts "Blindly assuming x86. If you get linker errors, this is why."
+              end
+
+$LDFLAGS += " -L #{LIBRARY_DIR} -Wl, -R. -Wl,-R./lib -l:discord_game_sdk#{
+                RUBY_PLATFORM.end_with?("linux") ? ".so" : ".dll"
+            }"
+
+create_makefile("rdiscord_sdk")
