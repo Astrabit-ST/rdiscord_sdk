010d96e166611a449d56358e4667b2cced35c356
Matthew Lyons <matthew@nowaffles.com>
2022-03-24 21:54:39 -0700

    Add activity type checking

--- ext/rdiscord_sdk/activity.h
+++ ext/rdiscord_sdk/activity.h
@@ -5,8 +5,11 @@
 
 using namespace Rice;
 
-#define GET_ACTIVITY(obj) \
- discord::Activity* activity = getPrivateData<discord::Activity>(obj);
-
 extern Class rb_cActivity;
 void rb_activity_init_class(Module module);
+
+#define GET_ACTIVITY(obj) \
+if (!obj.is_a(rb_cActivity)) { \
+    rb_raise(rb_eTypeError, "Activity is not a Discord::Activity"); \
+} \
+discord::Activity* activity = getPrivateData<discord::Activity>(obj);
